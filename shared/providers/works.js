07a887a1c7fa8d2a4e4cf4ae72eb9feae874de1e
Miguel Núñez Díaz-Montes <mnunezdm@gmail.com>
2021-06-05 19:18:48 +0200

    feat: Update data for works

--- shared/providers/works.js
+++ shared/providers/works.js
@@ -3,37 +3,160 @@ export default class WorkProvider {
     return [
       {
         id: "1",
-        title: "Obra 1",
+        title: "Reforma de estancia en chalet unifamiliar",
         description:
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus doloribus eligendi odio libero, magni neque. Ut saepe voluptas est nostrum asperiores! Necessitatibus sunt, fuga dolorem eligendi amet cumque quia dolorum.",
+          "Reforma de estancia en chalet, barra americana, cambio de puerta corredera por puertas.",
         images: [
           {
-            original: "https://picsum.photos/id/1018/1000/600/",
-            thumbnail: "https://picsum.photos/id/1018/250/150/",
+            original: "https://i.ibb.co/2sywmHk/nun1-post-01.jpg",
+            medium: "https://i.ibb.co/tx4SkFJ/nun1-post-01.jpg",
+            thumbnail: "https://i.ibb.co/5n6dtwR/nun1-post-01.jpg",
           },
           {
-            original: "https://picsum.photos/id/1015/1000/600/",
-            thumbnail: "https://picsum.photos/id/1015/250/150/",
+            original: "https://i.ibb.co/YXWsVjj/nun1-post-02.jpg",
+            medium: "https://i.ibb.co/G9JSrPP/nun1-post-02.jpg",
+            thumbnail: "https://i.ibb.co/zF5vtff/nun1-post-02.jpg",
           },
           {
-            original: "https://picsum.photos/id/1019/1000/600/",
-            thumbnail: "https://picsum.photos/id/1019/250/150/",
+            original: "https://i.ibb.co/8sj8QLg/nun1-post-03.jpg",
+            medium: "https://i.ibb.co/C8n0S3K/nun1-post-03.jpg",
+            thumbnail: "https://i.ibb.co/qjR123Y/nun1-post-03.jpg",
+          },
+          {
+            original: "https://i.ibb.co/VgNGzFd/nun1-post-04.jpg",
+            medium: "https://i.ibb.co/StxgMk2/nun1-post-04.jpg",
+            thumbnail: "https://i.ibb.co/MchHtzb/nun1-post-04.jpg",
           },
         ],
       },
       {
         id: "2",
-        title: "Obra 2",
+        title: "Reforma integral de vivienda en altura",
         description:
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus doloribus eligendi odio libero, magni neque. Ut saepe voluptas est nostrum asperiores! Necessitatibus sunt, fuga dolorem eligendi amet cumque quia dolorum.",
-        images: [],
+          "Reducción del número de habitaciones de 3 habitaciones a 2 habitaciones, dormitorio principal más habitación de invitados. Cocina americana con barra, ampliación de ventanas, eliminación del gotelé. Diseño por Camomila®",
+        images: [
+          {
+            original: "https://i.ibb.co/XS95xz2/nun2-post-02.jpg",
+            medium: "https://i.ibb.co/q5TGqj9/nun2-post-01.jpg",
+            thumbnail: "https://i.ibb.co/cL45f3t/nun2-post-01.jpg",
+          },
+          {
+            original: "https://i.ibb.co/2NG2VhP/nun2-post-01.jpg",
+            medium: "https://i.ibb.co/hDkdFK7/nun2-post-02.jpg",
+            thumbnail: "https://i.ibb.co/bzxv5rW/nun2-post-02.jpg",
+          },
+          {
+            original: "https://i.ibb.co/9rhCzFy/nun2-post-03.jpg",
+            medium: "https://i.ibb.co/6W8fV3D/nun2-post-03.jpg",
+            thumbnail: "https://i.ibb.co/sVCMk0s/nun2-post-03.jpg",
+          },
+          {
+            original: "https://i.ibb.co/vwqWnM0/nun2-post-04.jpg",
+            medium: "https://i.ibb.co/gj98Kck/nun2-post-04.jpg",
+            thumbnail: "https://i.ibb.co/X2LPRGc/nun2-post-04.jpg",
+          },
+          {
+            original: "https://i.ibb.co/KGy8Dp2/nun2-post-05.jpg",
+            medium: "https://i.ibb.co/MhNJfxc/nun2-post-05.jpg",
+            thumbnail: "https://i.ibb.co/RSHJNk3/nun2-post-05.jpg",
+          },
+          {
+            original: "https://i.ibb.co/JmdtgXQ/nun2-post-06.jpg",
+            medium: "https://i.ibb.co/rFt0Nns/nun2-post-06.jpg",
+            thumbnail: "https://i.ibb.co/8NBstFc/nun2-post-06.jpg",
+          },
+          {
+            original: "https://i.ibb.co/9q6yM8W/nun2-post-07.jpg",
+            medium: "https://i.ibb.co/GVyChxR/nun2-post-07.jpg",
+            thumbnail: "https://i.ibb.co/cN02mgQ/nun2-post-07.jpg",
+          },
+        ],
       },
       {
         id: "3",
-        title: "Obra 3",
+        title: "Reforma integral de sastreria",
         description:
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus doloribus eligendi odio libero, magni neque. Ut saepe voluptas est nostrum asperiores! Necessitatibus sunt, fuga dolorem eligendi amet cumque quia dolorum.",
-        images: [],
+          "Cambio de suelo, paredes, escaparates, baños, probadores...",
+        images: [
+          {
+            original: "https://i.ibb.co/Ldh88qr/hl-wh-01.jpg",
+            medium: "https://i.ibb.co/Ldh88qr/hl-wh-01.jpg",
+            thumbnail: "https://i.ibb.co/JjFKKbq/hl-wh-01.jpg",
+          },
+          {
+            original: "https://i.ibb.co/RgnHtB2/hl-wh-02.jpg",
+            medium: "https://i.ibb.co/RgnHtB2/hl-wh-02.jpg",
+            thumbnail: "https://i.ibb.co/9pXYBVn/hl-wh-02.jpg",
+          },
+          {
+            original: "https://i.ibb.co/fkY9dTs/hl-wh-03.jpg",
+            medium: "https://i.ibb.co/fkY9dTs/hl-wh-03.jpg",
+            thumbnail: "https://i.ibb.co/wSN7h2f/hl-wh-03.jpg",
+          },
+          {
+            original: "https://i.ibb.co/FWgNSyg/hl-post-01.jpg",
+            medium: "https://i.ibb.co/FWgNSyg/hl-post-01.jpg",
+            thumbnail: "https://i.ibb.co/1n8hDP8/hl-post-01.jpg",
+          },
+          {
+            original: "https://i.ibb.co/NFR6t8D/hl-post-02.jpg",
+            medium: "https://i.ibb.co/NFR6t8D/hl-post-02.jpg",
+            thumbnail: "https://i.ibb.co/3TKy4D6/hl-post-02.jpg",
+          },
+          {
+            original: "https://i.ibb.co/5x408p8/hl-post-03.jpg",
+            medium: "https://i.ibb.co/5x408p8/hl-post-03.jpg",
+            thumbnail: "https://i.ibb.co/yBP9yvy/hl-post-03.jpg",
+          },
+          {
+            original: "https://i.ibb.co/rxj1TrC/hl-post-04.jpg",
+            medium: "https://i.ibb.co/rxj1TrC/hl-post-04.jpg",
+            thumbnail: "https://i.ibb.co/R3FVZwx/hl-post-04.jpg",
+          },
+          {
+            original: "https://i.ibb.co/fYcfTm3/hl-post-05.jpg",
+            medium: "https://i.ibb.co/fYcfTm3/hl-post-05.jpg",
+            thumbnail: "https://i.ibb.co/3yJ2Ptx/hl-post-05.jpg",
+          },
+        ],
+      },
+      {
+        id: "4",
+        title: "Reforma integral de vivienda en altura",
+        description:
+          "Reforma integral de vivienda en altura en madrid centro. Cambio de suelos, reforma de baño, reforma de cocina",
+        images: [
+          {
+            original: "https://i.ibb.co/gdQmnTX/pe-post-01.jpg",
+            medium: "https://i.ibb.co/gdQmnTX/pe-post-01.jpg",
+            thumbnail: "https://i.ibb.co/FXcmPwf/pe-post-01.jpg",
+          },
+          {
+            original: "https://i.ibb.co/DQ5mg8b/pe-post-02.jpg",
+            medium: "https://i.ibb.co/DQ5mg8b/pe-post-02.jpg",
+            thumbnail: "https://i.ibb.co/TtmJqbT/pe-post-02.jpg",
+          },
+          {
+            original: "https://i.ibb.co/1J5Btkj/pe-post-03.jpg",
+            medium: "https://i.ibb.co/1J5Btkj/pe-post-03.jpg",
+            thumbnail: "https://i.ibb.co/NnPfdMb/pe-post-03.jpg",
+          },
+          {
+            original: "https://i.ibb.co/p1JN4Ys/pe-post-04.jpg",
+            medium: "https://i.ibb.co/p1JN4Ys/pe-post-04.jpg",
+            thumbnail: "https://i.ibb.co/tH8nYVN/pe-post-04.jpg",
+          },
+          {
+            original: "https://i.ibb.co/JrnTjnH/pe-post-05.jpg",
+            medium: "https://i.ibb.co/JrnTjnH/pe-post-05.jpg",
+            thumbnail: "https://i.ibb.co/qmB8YBp/pe-post-05.jpg",
+          },
+          {
+            original: "https://i.ibb.co/gyv2mSR/pe-post-06.jpg",
+            medium: "https://i.ibb.co/gyv2mSR/pe-post-06.jpg",
+            thumbnail: "https://i.ibb.co/wyJkBSz/pe-post-06.jpg",
+          },
+        ],
       },
     ];
   }
